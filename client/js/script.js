e69401bc40b33e1ee0383860ea45577474b0ef92
Dhruv Jyoti Das <88588328+DhruvJyotiDas@users.noreply.github.com>
2025-05-05 09:43:34 +0530

    Update script.js

--- client/js/script.js
+++ client/js/script.js
@@ -192,7 +192,7 @@ async function handleSocketMessage(event) {
 
 async function summarizeMessage(originalText) {
   try {
-    const res = await fetch("https://145b-2401-4900-634f-1e7e-75c4-f53-706e-7177.ngrok-free.app/summarize", {  // ✅ No change yet
+    const res = await fetch("   https://6bd9-2401-4900-634f-1e7e-75c4-f53-706e-7177.ngrok-free.app", {  // ✅ No change yet
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ message: originalText }),
